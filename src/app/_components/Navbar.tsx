610e52b792476717661bee41b22b8c548d7cac27
Prateek Kumar <prateek.kumar.ind@gmail.com>
2026-03-17 11:12:22 +0530

    create navbar component

---
+++ src/app/_components/Navbar.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import React from 'react'
+import LogoutButton from './LogoutButton'
+
+const Navbar = () => {
+    const session = true
+  return (
+    <nav className='bg-white shadow-sm'>
+        <div className='container mx-auto p-4 flex justify-between items-center'>
+            <Link href="/" className='text-xl font-bold text-gray-600 hover:text-gray-800'>
+                Contact Manager
+            </Link>
+            <div className='flex items-center space-x-4'>
+                {session ? (
+                    <>
+                    <Link href={"/contact"} className='hover:text-gray-800 mr-8'>
+                        Contacts
+                    </Link>
+                    <LogoutButton/>
+                    </>
+                ): (
+                    <>
+                    <Link href={'/login'} className='hover:text-gray-800'>
+                        Login
+                    </Link>
+                    <Link href={'/register'} className='hover:text-gray-800'>
+                        Register
+                    </Link>
+                    </>
+                )}
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
