eae317964e1c9774794cf477611575edc4bbf280
Prateek Kumar <prateek.kumar.ind@gmail.com>
2026-03-19 15:37:11 +0530

    create edit contact page

--- src/app/contact/edit/[id]/page.tsx
+++ src/app/contact/edit/[id]/page.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import ContactForm from '@/app/_components/ContactForm'
+import { editContactAction } from '../../../actions/contacts'
+import { getContactsById } from '../../../api/contacts'
 
-const EditContactPage = () => {
+
+ 
+
+const EditContactPage = async({params}: {params: Promise<{id: string}> }) => {
+  const {id} = await params
+  const contact = await getContactsById(id)
   return (
-    <div>EditContactPage</div>
+     <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
+      <h1 className="font-bold">Edit Contact</h1>
+      <ContactForm  action={editContactAction} contact={contact}/>
+    </div>
   )
 }
 
