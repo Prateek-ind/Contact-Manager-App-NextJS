f317906c3afa81c06d483968ed7db7102fdff444
Prateek Kumar <prateek.kumar.ind@gmail.com>
2026-03-27 11:31:37 +0530

    correct spelling mistake

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const bycrypt = require("bcryptjs")
-console.log("bcrypt:", bycrypt)
+const bcrypt = require("bcryptjs")
 
 const userSchema = new mongoose.Schema(
   {
@@ -29,11 +28,11 @@ userSchema.pre("save", async function () {
    console.log("pre save hook fired")
   
   if (!this.isModified("password")) return ;
-  this.password = await bycrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
-  return bycrypt.compare(candidatePassword, this.password);
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
 const userModel = mongoose.model("user", userSchema);
